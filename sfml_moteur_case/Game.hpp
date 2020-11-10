29bbb1d0e3b3e57edf62f7d0a3ebec6573ffd228
delahee <david.elahee@gmail.com>
2020-11-10 15:16:54 +0100

    commit moteur case

---
+++ sfml_moteur_case/Game.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <vector>
+
+#include "SFML/Graphics.hpp"
+#include "SFML/System.hpp"
+#include "SFML/Window.hpp"
+
+#include "Particle.hpp"
+#include "ParticleMan.hpp"
+#include "Turtle.hpp"
+
+
+using namespace sf;
+
+class Game {
+public:
+
+	sf::RenderWindow*	win = nullptr;
+	Turtle				turtle;
+
+	sf::RectangleShape  bg;
+	sf::Texture			tex;
+
+	bool				closing = false;
+
+	ParticleMan beforeParts;
+	ParticleMan afterParts;
+
+	Game(sf::RenderWindow * win) {
+		this->win = win;
+		bg = sf::RectangleShape(Vector2f(win->getSize().x, win->getSize().y));
+		
+		bool isOk = tex.loadFromFile("res/bg.jpg");
+		if (!isOk) {
+			printf("ERR : LOAD FAILED\n");
+		}
+		bg.setTexture(&tex);
+		bg.setSize(sf::Vector2f(1280, 720));
+	}
+
+	void processInput(sf::Event ev);
+
+	bool wasPressed = false;
+
+
+	void pollInput(double dt);
+	void onSpacePressed();
+
+	void update(double dt);
+
+	void draw(sf::RenderWindow& win);
+
+	void parse();
+	void onFileTick();
+};
